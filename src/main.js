5615ce7a379727aeb2200efdce8456787cc5aed6
ClickD <michaelwd@yeah.net>
2020-10-14 19:16:49 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 // 加载全局样式
 import './styles/index.less'
 
+// vant 全局组件库
+import Vant from 'vant'
+// 加载 vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用 vant 组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
